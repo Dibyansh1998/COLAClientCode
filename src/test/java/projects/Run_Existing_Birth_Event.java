5bc5013d711a82cae3bd75a0580b107bc5c3b02d
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-04-23 13:45:22 +0530

    New Code Added

---
+++ src/test/java/projects/Run_Existing_Birth_Event.java
@@ -0,0 +1,169 @@
+package projects;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Run_Existing_Birth_Event {
+
+	static WebDriver driver;
+	static WebDriverWait wait;
+	 
+
+	public static void main(String[] args) throws InterruptedException {
+		Launch_COLA_Site();
+		LogIn_Page();
+		Authorization_Page();
+		LifeEventExecution();
+		ClickMakeElectionButton();
+		DependentScreenData();
+		Tire_Down();
+
+	}
+
+	// Below Command to use to Launch the Browser and Open your Client Site
+	@BeforeTest
+	public static void Launch_COLA_Site() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		// Below Command to put the URL:
+		driver.get("https://pp.lifeatworkportal.com/countyofla");
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+	}
+
+	// Below Command to use to put your UserName and Password and then click
+	// LogIn-Button
+	@Test
+	public static void LogIn_Page() throws InterruptedException {
+
+		// Log in page Commands:
+		driver.findElement(By.id("USERNAME")).sendKeys("911317101");
+		driver.findElement(By.id("password")).sendKeys("Test@1234");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("showhide")));
+		driver.findElement(By.id("showhide")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("ContinueBtn")).click();
+	}
+
+	// This Code will be execute to check the Authorization page
+	@Test
+	public static void Authorization_Page() throws InterruptedException {
+		// Authorization Page Commands till Home page:
+		driver.findElement(By.id("Email")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("ContinueBtn")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("seccode")).sendKeys("64769644");
+		driver.findElement(By.id("showhide")).click();
+		driver.findElement(By.id("Device1")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("ContinueBtn")).click();
+		Thread.sleep(3000);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='col-md-12'] h3")).getText(),
+				"Authorization Confirmed");
+		System.out.println(driver.findElement(By.cssSelector("div[class='col-md-12'] h3")).getText());
+		driver.findElement(By.id("ContinueBtn")).click();
+
+	}
+
+	// This below code will be allowed Cookies Pop-up and Click on Life Event Tab
+	@Test
+	public static void LifeEventExecution() throws InterruptedException {
+		Thread.sleep(1500);
+		wait.until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='Accept Cookies']")))
+				.click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[normalize-space()='Life Events']")).click();
+
+	}
+
+	@Test
+	public static void ClickMakeElectionButton() throws InterruptedException {
+
+		Thread.sleep(9000);
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//span[@class='makeElectionBut ng-scope ng-isolate-scope']")))
+				.click();
+		Thread.sleep(14000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,700)", "");
+		Thread.sleep(3000);
+		js.executeScript("window.scrollTo(0,200)", "");
+		//Below line of code will click on Change button of Medical Benefit
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@name='leEnrollChange'])[1]"))).click();
+		js.executeScript("window.scrollTo(0,1200)","");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[@class='glyphicons glyphicons-plus-sign'])[1]"))).click();
+		js.executeScript("window.scrollTo(0,800)","");
+	}
+	@Test
+	public static void DependentScreenData() throws InterruptedException
+	{
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='First Name']"))).sendKeys("Assane");
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//input[@placeholder='Middle Name']")).sendKeys("Michele");
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Lupin");
+		Thread.sleep(1500);
+		JavascriptExecutor js1= (JavascriptExecutor)driver;
+		js1.executeScript("window.scrollTo(0,1200)","");
+		Thread.sleep(1000);
+		js1.executeScript("window.scrollTo(0,300)","");
+		driver.findElement(By.cssSelector(".glyphicon.glyphicon-calendar")).click();
+		Thread.sleep(1500);
+//		driver.findElement(By.xpath("//div[@class='datetimepicker-years']//th[@class='prev']")).click();
+		for(int i=0;i<2;i++)
+		{
+			driver.findElement(By.xpath("//div[@class='datetimepicker-years']//th[@class='prev']")).click();
+		}
+		
+		driver.findElement(By.xpath("//span[normalize-space()='2001']")).click();
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//span[normalize-space()='Mar']")).click();
+		Thread.sleep(1500);
+		driver.findElement(By.cssSelector("tbody tr:nth-child(2) td:nth-child(3)")).click();
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//input[@name='ssn']")).sendKeys("911311000");
+		Thread.sleep(1500);
+		
+		//Select the Gender
+		WebElement genderOptions=driver.findElement(By.xpath("//select[@name='gender']"));
+		Select genderdropdown= new Select(genderOptions);
+		genderdropdown.selectByIndex(1);
+		System.out.println(genderdropdown.getFirstSelectedOption().getText());
+		
+		//Select the Relation
+		WebElement relationshipcode=driver.findElement(By.xpath("//select[@name='relation']"));
+		Select relationcode= new Select(relationshipcode);
+		relationcode.selectByIndex(2);
+		System.out.println(relationcode.getFirstSelectedOption().getText());
+		
+		// Click on the Save button on Dependent Screen
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@name='leMakeElections_AddUpdateDependentFamilyMember']")).click();
+	}
+
+	// This code would be Close the Browser
+	@AfterTest
+	public static void Tire_Down() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.close();
+	}
+
+}
--- src/test/java/projects/Run_New_Birth_Event.java
+++ src/test/java/projects/Run_New_Birth_Event.java
@@ -3,6 +3,7 @@ package projects;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,7 +15,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Birth_Event {
+public class Run_New_Birth_Event {
 
 	static WebDriver driver;
 	static WebDriverWait wait;
@@ -23,8 +24,10 @@ public class Birth_Event {
 		Launch_COLA_Site();
 		LogIn_Page();
 		Authorization_Page();
-		Accept_Cookies();
-		Tire_Down();
+		LifeEventExecution();
+		ClickLifeEvent();
+		DatePicker();
+//		Tire_Down();
 
 	}
 
@@ -38,14 +41,16 @@ public class Birth_Event {
 		// Below Command to put the URL:
 		driver.get("https://pp.lifeatworkportal.com/countyofla");
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
 	}
 
-	// Below Command to use to put your UserName and Password and then click LogIn-Button
+	// Below Command to use to put your UserName and Password and then click
+	// LogIn-Button
 	@Test
 	public static void LogIn_Page() throws InterruptedException {
 
 		// Log in page Commands:
-		driver.findElement(By.id("USERNAME")).sendKeys("911317101");
+		driver.findElement(By.id("USERNAME")).sendKeys("911317102");
 		driver.findElement(By.id("password")).sendKeys("Test@1234");
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("showhide")));
 		driver.findElement(By.id("showhide")).click();
@@ -53,7 +58,7 @@ public class Birth_Event {
 		driver.findElement(By.id("ContinueBtn")).click();
 	}
 
-	//This Code will be execute to check the Authorization page
+	// This Code will be execute to check the Authorization page
 	@Test
 	public static void Authorization_Page() throws InterruptedException {
 		// Authorization Page Commands till Home page:
@@ -74,15 +79,40 @@ public class Birth_Event {
 
 	}
 
-	//This below code will be allowed Cookies Pop-up
+	// This below code will be allowed Cookies Pop-up and Click on Life Event Tab
 	@Test
-	public static void Accept_Cookies() {
+	public static void LifeEventExecution() throws InterruptedException {
 		wait.until(
 				ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='Accept Cookies']")))
 				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[normalize-space()='Life Events']")).click();
+
 	}
 
-	//This code would be Close the Browser
+	@Test
+	public static void ClickLifeEvent() throws InterruptedException {
+
+		Thread.sleep(10000);
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Birth/Adoption of Child #7']"))).click();
+		Thread.sleep(7000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,800)", "");
+		Thread.sleep(4000);
+
+	}
+
+	@Test
+	public static void DatePicker() throws InterruptedException {
+		driver.findElement(By.cssSelector(".glyphicon.glyphicon-calendar")).click();
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tbody//tr//td[@class='day active']"))).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[@class='ng-scope ng-isolate-scope'][normalize-space()='Continue']")).click();
+	}
+
+	// This code would be Close the Browser
 	@AfterTest
 	public static void Tire_Down() throws InterruptedException {
 		Thread.sleep(2000);
