fd80fbf2e286aa44a6903d633305bf2f598cf673
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-04-25 19:40:11 +0530

    Code Added

---
+++ src/test/java/projects/SwappingNumbers.java
@@ -0,0 +1,21 @@
+package projects;
+
+public class SwappingNumbers {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println("Before Swapping: A= "+a+" B= "+b+"");
+		
+		int temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("After Swapping: A= "+a+" B= "+b+"");
+		
+
+	}
+
+}
