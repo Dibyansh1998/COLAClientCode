a0b379ef2101ea18f99bb15afe9546bddd0ff3ec
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-06-05 19:58:33 +0530

    SSNN Updated again

--- src/test/java/projects/Electmaint_Make_Election.java
+++ src/test/java/projects/Electmaint_Make_Election.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import ru.yandex.qatools.ashot.AShot;
@@ -22,18 +23,18 @@ import ru.yandex.qatools.ashot.Screenshot;
 import ru.yandex.qatools.ashot.coordinates.WebDriverCoordsProvider;
 import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
+@Test(groups="MakeElectionClass")
 public class Electmaint_Make_Election {
 
-	public static void main(String[] args) throws InterruptedException, IOException {
-
+	public void TakeMakeElectionScreenShot() throws InterruptedException, IOException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
+		
 		// Calendar
-
 		Calendar cal = Calendar.getInstance();
 		Date time = cal.getTime();
 		String timestamp = time.toString().replace(":", "_").replace(" ", "_");
@@ -41,21 +42,21 @@ public class Electmaint_Make_Election {
 		String[][] credentials = {
 
 				{ "911316451", "Test@1234" }, // 911316451:ASEMGN
-				{ "911316452", "Test@1234" }, // 911316452:TSEMGN
-				{ "911316453", "Test@1234" }, // 911316453:ASEMCN
-				{ "911316454", "Test@1234" }, // 911316454:TTEMGN
-				{ "911316455", "Test@1234" }, // 911316455:RSEMGN
-				{ "911316456", "Test@1234" }, // 911316456:TSEMCN
-				{ "911316457", "Test@1234" }, // 911316457:ASMDGN
-				{ "911316458", "Test@1234" }, // 911316458:ASPPGN
-				{ "911316459", "Test@1234" }, // 911316459:RSPPGN
-				{ "911316460", "Test@1234" }, // 911316460:TSEMGW
-				{ "911316461", "Test@1234" }, // 911316461:PSEMGN
-				{ "911316462", "Test@1234" }, // 911316462:RSMDGN
-				{ "911316463", "Test@1234" }, // 911316463:OSEMGN
-				{ "911316464", "Test@1234" }, // 911316464:CSEMGC
-				{ "911316465", "Test@1234" }, // 911316465:TSMDGN
-				{ "911316466", "Test@1234" }, // 911316466:TSPPGN
+//				{ "911316452", "Test@1234" }, // 911316452:TSEMGN
+//				{ "911316453", "Test@1234" }, // 911316453:ASEMCN
+//				{ "911316454", "Test@1234" }, // 911316454:TTEMGN
+//				{ "911316455", "Test@1234" }, // 911316455:RSEMGN
+//				{ "911316456", "Test@1234" }, // 911316456:TSEMCN
+//				{ "911316457", "Test@1234" }, // 911316457:ASMDGN
+//				{ "911316458", "Test@1234" }, // 911316458:ASPPGN
+//				{ "911316459", "Test@1234" }, // 911316459:RSPPGN
+//				{ "911316460", "Test@1234" }, // 911316460:TSEMGW
+//				{ "911316461", "Test@1234" }, // 911316461:PSEMGN
+//				{ "911316462", "Test@1234" }, // 911316462:RSMDGN
+//				{ "911316463", "Test@1234" }, // 911316463:OSEMGN
+//				{ "911316464", "Test@1234" }, // 911316464:CSEMGC
+//				{ "911316465", "Test@1234" }, // 911316465:TSMDGN
+//				{ "911316466", "Test@1234" }, // 911316466:TSPPGN
 
 		};
 
@@ -101,31 +102,22 @@ public class Electmaint_Make_Election {
 					ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[normalize-space()='expand all']")))
 					.click();
 
-//			// Specify the destination folder path
-//			String folderPath = "C:\\Users\\52304535\\OneDrive - Conduent\\Documents\\OneNote Notebooks\\Screenshots\\"
-//					+ username + "\\";
-//
-//			// Create the folder if it doesn't exist
-//			File folder = new File(folderPath);
-//			if (!folder.exists()) {
-//				folder.mkdirs(); // Create folder and parent directories if necessary
-//			}
-
+			Thread.sleep(2000);
 			// Take ScreenShot Command
 			WebElement ele = wait.until(ExpectedConditions
-					.visibilityOfElementLocated(By.xpath("//div[@class='row makeElectionPageRow ng-scope']")));
+					.visibilityOfElementLocated(By.xpath("//div[@class='row makeElectionPageRow ng-scope']//div[@class='row']")));
 
 			// Ensure the element is within the viewport
 			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ele);
-			Thread.sleep(1000);
+			Thread.sleep(2000);
 			Screenshot screenshot = new AShot().coordsProvider(new WebDriverCoordsProvider())
 					.shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver, ele);
 
 			Thread.sleep(1000);
 			// Specify the destination where the screenshot will be saved
 
-			File destination = new File(
-					System.getProperty("user.dir") + "ScreenShots\\snagit_" + timestamp + "_" + username + ".png");
+			File destination = new 
+					File("C:\\Desktop Items\\Attachments\\snagits_" + timestamp + "_" + username + ".png");
 
 			// Save the screenshot to the specified file
 			Thread.sleep(1000);
@@ -157,4 +149,5 @@ public class Electmaint_Make_Election {
 		// Quit the driver
 		driver.quit();
 	}
+
 }
